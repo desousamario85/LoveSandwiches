fc37d33aea2cc884fb4eebbed6cfc5fe3c41f806
desousamario85 <desousa.mario@yahoo.com>
2022-03-31 21:16:25 +0000

    Refactored update functions

--- run.py
+++ run.py
@@ -9,19 +9,17 @@ SCOPE = [
     ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
-SCOPE_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
-
-data = sales.get_all_values()
-
-print(data)
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -29,56 +27,55 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
-        
+
         sales_data = data_str.split(",")
+
         if validate_data(sales_data):
-            print("Data is valid")
+            print("Data is valid!")
             break
+
     return sales_data
+
 
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
-    Raises ValueError if srting cannot be convert into int, 
-    pr if there aren't exactly 6 values
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
-
     try:
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
-                )
+            )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
+
     return True
 
-def update_sales_worksheet(data):
-    """
-    Updating sales workshee, add new row with the list data provided
-    """
-    print("Updating sales worksheet.../n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated")
 
-
-def update_surplus_worksheet(new_surplus_data):
+def update_worksheet(data, worksheet):
     """
-    Updating surplus workshee, add new row with the list data provided
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
     """
-    print("Updating surplus worksheet.../n")
-    sales_worksheet = SHEET.worksheet("surplus")
-    sales_worksheet.append_row(new_surplus_data)
-    print("Surplus worksheet updated")
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
 def calculate_surplus_data(sales_row):
     """
-    Compares sales with stock
+    Compare sales with stock and calculate the surplus for each item type.
+
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
     """
-    print("Calculating surplus data.../n")
+    print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
     
@@ -90,18 +87,16 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
-
 def main():
     """
     Run all program functions
     """
-
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 
-print("Welcome to Love Sandwiches data automation")
+print("Welcome to Love Sandwiches Data Automation")
 main()
